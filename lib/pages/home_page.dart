cdc377f701e2fb2b85ec508205347c2d8d090307
Kyara Araújo <araujomrk@gmail.com>
2022-10-22 20:26:51 -0300

    refactor list of pages and add contact page to it.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:layouts/pages/contact_page.dart';
 import 'package:layouts/pages/dessert_page.dart';
 import 'package:layouts/pages/weather_page.dart';
 
@@ -7,55 +8,58 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const List<String> pagesTitle = [
-      'Dessert Page with favorite state',
-    ];
+    ;
 
-    const List<Widget> pagesView = [
-      DessertPage(),
+    const List<Map<String, dynamic>> pages = [
+      {'label': 'Dessert - favorite interaction', 'page': DessertPage()},
+      {'label': 'Contact', 'page': ContactPage()},
+      {'label': 'Weather', 'page': WeatherPage()},
     ];
 
     Color? alternateCardColor(int index) {
       if (index.isEven) {
-        return Colors.blue[600];
+        return Colors.black87;
       } else {
-        return Colors.blue[300];
+        return Colors.black;
       }
     }
 
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Layouts for practicing',
-      home: WeatherPage(),
-      // home: Scaffold(
-      //   appBar: AppBar(
-      //     title: const Text('Layouts Pages'),
-      //   ),
-      // body: ListView.separated(
-      //   padding: const EdgeInsets.all(8),
-      //   separatorBuilder: (context, index) => const Divider(),
-      //   itemCount: pagesTitle.length,
-      //   itemBuilder: (context, index) {
-      //     return Card(
-      //       elevation: 5,
-      //       child: ListTile(
-      //         tileColor: alternateCardColor(index),
-      //         textColor: Colors.grey[100],
-      //         title: Text(pagesTitle[index]),
-      //         leading: Text('${index + 1}'),
-      //         shape: RoundedRectangleBorder(
-      //             borderRadius: BorderRadius.circular(10)),
-      //         onTap: () {
-      //           Navigator.push(
-      //               context,
-      //               MaterialPageRoute(
-      //                 builder: (context) => pagesView[index],
-      //               ));
-      //         },
-      //       ),
-      //     );
-      //   },
-      // ),
-      // ),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch().copyWith(primary: Colors.black87),
+      ),
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade400,
+        appBar: AppBar(
+          title: const Text('Layouts Pages'),
+        ),
+        body: ListView.separated(
+          padding: const EdgeInsets.all(8),
+          separatorBuilder: (context, index) => const Divider(),
+          itemCount: pages.length,
+          itemBuilder: (context, index) {
+            return Card(
+              elevation: 5,
+              child: ListTile(
+                tileColor: alternateCardColor(index),
+                textColor: Colors.grey[100],
+                title: Text(pages[index]['label']),
+                leading: Text('${index + 1}'),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => pages[index]['page'],
+                      ));
+                },
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
